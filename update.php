f763c8522f5752efa425a8243bf103730ef362a2
jaakko.lukkari@gmail.com <jaakko.lukkari@gmail.com@f6f80a8b-968f-e7cd-b104-9312d2818bcb>
2011-02-02 11:51:10 +0000

    git-svn-id: http://xelisaviihde.googlecode.com/svn/trunk@29 f6f80a8b-968f-e7cd-b104-9312d2818bcb

---
+++ update.php
@@ -0,0 +1,67 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+
+
+require_once('libs/pclzip.lib.php');
+
+$updateInfoUrl="http://xelisaviihde.googlecode.com/svn/wiki/UpdateInfo.wiki";
+$currentVersionInfo="version.xml";
+
+$updateXml=file_get_contents($updateInfoUrl);
+
+
+if(file_exists($currentVersionInfo)) {
+$curdoc = new DOMDocument();
+$curdoc->load( $currentVersionInfo );
+$currentVersion = $curdoc->getElementsByTagName( "version" )->item(0)->nodeValue;
+} else {
+$currentVersion="0.0.0";	
+}
+
+$newdoc = new DOMDocument();
+$newdoc->loadXML( $updateXml );
+$newestVersion = $newdoc->getElementsByTagName( "version" )->item(0)->nodeValue;
+$downloadUrl=$newdoc->getElementsByTagName( "url" )->item(0)->nodeValue;
+$changeLog=$newdoc->getElementsByTagName( "changeLog" )->item(0)->nodeValue;
+
+$currentVersion = $currentVersion;
+$newestVersion = $newestVersion;
+
+if(version_compare($currentVersion, $newestVersion, '<')) {
+	
+	//get file
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $downloadUrl);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_FAILONERROR, true);
+    curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+    curl_setopt($ch, CURLOPT_BINARYTRANSFER,true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $file = curl_exec($ch);
+    curl_close($ch);
+    $fp = fopen('updateTempFile.zip', 'w');
+    fwrite($fp, $file);
+    fclose($fp);
+	
+	
+	$archive = new PclZip('updateTempFile.zip');
+    
+	if ($archive->extract(PCLZIP_OPT_ADD_PATH, '.',  PCLZIP_OPT_REMOVE_PATH, 'xelisaviihde') == 0) {
+	unlink('updateTempFile.zip');
+
+		
+				//Päivitys epäonnistui
+		
+	} else {
+		
+	unlink('updateTempFile.zip'');
+			//päivitys onnistui
+	}
+	
+	
+	
+}
+  
+  
