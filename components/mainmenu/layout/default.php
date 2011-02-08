ca1507aefa59f767cccd235e16488b8fea64aafb
jaakko.lukkari@gmail.com <jaakko.lukkari@gmail.com@f6f80a8b-968f-e7cd-b104-9312d2818bcb>
2011-02-08 11:20:17 +0000

    git-svn-id: http://xelisaviihde.googlecode.com/svn/trunk@45 f6f80a8b-968f-e7cd-b104-9312d2818bcb

--- components/mainmenu/layout/default.php
+++ components/mainmenu/layout/default.php
@@ -25,6 +25,16 @@ echo  "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n";
 ?>
 <rss version="2.0"  xmlns:media="http://purl.org/dc/elements/1.1/"  xmlns:dc="http://purl.org/dc/elements/1.1/">
 
+<onEnter>
+		
+	/* start rtmpgw */
+	ret = getURL("");
+</onEnter>
+
+<onExit>
+	/* stop rtmpgw */
+	ret = getURL("");
+</onExit>
 
  <mediaDisplay  name="photoView"
     imageBottomSide="null"
